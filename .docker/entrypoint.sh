70946b9f24e904195b0165d213f197b5f9f1df05
jcobarreto <julio.barreto@gmail.com>
2020-06-01 18:44:59 -0300

    Incluindo instalação do composer no entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl -sS https://getcomposer.org/installer | php -- -- install-dir=/usr/local/bin --filename=composer
 ./composer install
 php artisan key:generate
 php artisan migrate
