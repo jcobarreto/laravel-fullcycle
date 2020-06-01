1ce42ea9c424e12aa370cecd345a9b27234f0666
jcobarreto <julio.barreto@gmail.com>
2020-06-01 20:42:35 -0300

    Alteração caminho composer

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,6 +2,7 @@
 
 curl -sS https://getcomposer.org/installer | php -- -- install-dir=/usr/local/bin --filename=composer
 cp /var/www/composer /usr/local/bin
+chmod 777 /usr/loca/bin/composer
 
 ./composer install
 php artisan key:generate
